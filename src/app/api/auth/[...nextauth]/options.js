20de12b7873806795c2815663d2834a64b9ae412
miguelbidarrab <miguelbidarrab.com>
2024-05-20 17:55:23 +0100

    Changed options.js

--- src/app/api/auth/[...nextauth]/options.js
+++ src/app/api/auth/[...nextauth]/options.js
@@ -1,18 +1,17 @@
-import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 import User from "@/app/(models)/User";
 import bcrypt from "bcrypt";
 
-export const options = {
+export const authOptions = {
   providers: [
     GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
       profile(profile) {
         console.log("Profile Google: ", profile);
-
         let userRole = "member";
-
-        if (profile?.email == "miguelbidarrab@gmail.com") {
+        if (profile?.email === "miguelbidarrab@gmail.com") {
           userRole = "admin";
         }
         return {
@@ -21,47 +20,27 @@ export const options = {
           role: userRole,
         };
       },
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-      callbackUrl: "/api/auth/callback/google",
     }),
     CredentialsProvider({
       name: "Credentials",
       credentials: {
-        email: {
-          label: "email:",
-          type: "text",
-          placeholder: "your-email",
-        },
-        password: {
-          label: "password:",
-          type: "password",
-          placeholder: "your-password",
-        },
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
       },
       async authorize(credentials) {
         try {
-          const foundUser = await User.findOne({ email: credentials.email })
-            .lean()
-            .exec();
-
+          // Using lean() to optimize Mongoose query
+          const foundUser = await User.findOne({ email: credentials.email }).lean().exec();
           if (foundUser) {
-            console.log("User Exists");
-            const match = await bcrypt.compare(
-              credentials.password,
-              foundUser.password
-            );
-
+            const match = await bcrypt.compare(credentials.password, foundUser.password);
             if (match) {
-              console.log("Good Pass");
               delete foundUser.password;
-
               foundUser["role"] = "Member";
               return foundUser;
             }
           }
         } catch (error) {
-          console.log(error);
+          console.error("Error in authorization: ", error);
         }
         return null;
       },
@@ -82,3 +61,5 @@ export const options = {
     },
   },
 };
+
+export default NextAuth(authOptions);
